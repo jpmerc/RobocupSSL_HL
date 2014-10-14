06fa1292848ce9756bbdf20721a4138c58dac63f
Félix Pelletier <felixpelletier23@gmail.com>
2014-10-14 16:11:36 -0400

    Python POC: Added junk code to simulate intensive code in position.py

--- python/position.py
+++ python/position.py
@@ -3,6 +3,14 @@ import math
 
 
 def getPosition(t):
+
+    # junk code to simulate real complex code
+    j = 0
+    for i in range(1000):
+        j = math.cos(i**42)
+        math.sin(j)
+
+    # real code
     x = math.cos(t/1000)
     y = math.sin(t/1000)
     return (x, y)
