e84346b95d5dc245db572278b6cac1c7fcdf1245
Félix Pelletier <felixpelletier23@gmail.com>
2014-10-23 15:47:30 -0400

    Python POC: Enabled profiling

--- python/run.sh
+++ python/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-g++ -I /usr/include/SDL -I/usr/include/python3.4m -o main -Wall main.cpp script.cpp -lSDL -lSDL_image -lpython3.4m -lboost_python3 -lboost_system -lboost_thread && ./main
+g++ -I /usr/include/SDL -I/usr/include/python3.4m -o main -Wall -pg main.cpp script.cpp -lSDL -lSDL_image -lpython3.4m -lboost_python3 -lboost_system -lboost_thread && ./main
+
 
